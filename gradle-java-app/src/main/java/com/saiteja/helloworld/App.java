5e4abee41c6f2978698e2b54f471302a49cc43bd
root <root@ip-172-31-31-68.eu-north-1.compute.internal>
2025-08-30 05:12:18 +0000

    first commit

---
+++ gradle-java-app/src/main/java/com/saiteja/helloworld/App.java
@@ -0,0 +1,7 @@
+package com.saiteja.helloworld;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! from Gradle Java Project 🚀");
+    }
+}
